ca989f1fc3a81c3ea0c880bbfbccd346ede981db
DVanhoan <144916089+DVanhoan@users.noreply.github.com>
2024-11-29 16:39:26 +0700

    create migrate message

---
+++ resources/views/message/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.message')
+@section('content')
+<section class="job-section">
+  <div id="message"></div>
+  <script src="{{ mix('js/app.js') }}"></script>
+</section>
+@endsection
